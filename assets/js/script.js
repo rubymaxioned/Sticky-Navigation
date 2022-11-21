71c07f9ced2341605cece03f6340a61be4567669
rubymaxioned <axioned0482022>
2022-11-21 10:49:05 +0530

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 var header = document.querySelector("header");
 var att = document.querySelectorAll(".nav-links a");
-console.log(att);
-console.log(header);
+
 window.addEventListener('scroll',myFunction);
 function myFunction(){
     if (window.scrollY > 500) {
-        header.style.backgroundColor = "white";
+        header.style.backgroundColor = "#fff";
         att.forEach(function(list){
             list.classList.add('color');
         })
     }  
     else {
-        header.style.backgroundColor = "black";
+        header.style.backgroundColor = "#000";
         att.forEach(function(list){
             list.classList.remove('color');
         })
