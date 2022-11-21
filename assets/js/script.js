1050f24a198ad91adc2f746a6dd886964c3e0e9d
rubymaxioned <axioned0482022>
2022-11-21 10:15:56 +0530

    added scroll function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 var header = document.querySelector("header");
 console.log(header);
-if (window.scrollY > 300) {
-    header.style.backgroundColor = "white";
+window.addEventListener('scroll',myFunction);
+function myFunction(){
+    if (window.scrollY > 500) {
+        header.style.backgroundColor = "white";
+    }  
+    else {
+        header.style.backgroundColor = "black";
+    }
 }
